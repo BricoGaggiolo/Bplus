222af60eb0c5ce49b6b3686a6ba0e8c8dcd71b60
Bplus <bricogaggiolo@laquattrosrl.it>
2026-09-25 13:59:23 +0200

    💀 SW kill: si auto-disinstalla e cancella tutto

--- sw.js
+++ sw.js
@@ -1,19 +1,16 @@
-/* B+ Gestionale SW - v5 no-cache */
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(
-  caches.keys().then(k => Promise.all(k.map(c => caches.delete(c))))
-    .then(() => self.clients.claim())
-));
-/* Nessun intercettamento fetch: lascia tutto al browser */
-self.addEventListener('push', e => {
-  const d = e.data ? e.data.json() : {};
-  e.waitUntil(self.registration.showNotification(d.title||'B+ Gestionale',{
-    body: d.body||'', icon:'/Bplus/icons/icon-192.png',
-    badge:'/Bplus/icons/icon-72.png', tag: d.tag||'bplus',
-    data:{ url: d.url||'/Bplus/' }
-  }));
+/* SW kill - si disinstalla da solo e cancella tutto */
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.skipWaiting())
+  );
 });
-self.addEventListener('notificationclick', e => {
-  e.notification.close();
-  e.waitUntil(clients.openWindow(e.notification.data?.url||'/Bplus/'));
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+      .then(() => self.registration.unregister())
+  );
 });
